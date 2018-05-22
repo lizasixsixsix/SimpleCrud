f28855ce40236de78a285c139f7fb2517e54cf24
lizasixsixsix <lizasixsixsix@gmail.com>
2018-05-22 16:15:05 +0300

    added to remote

---
+++ SimpleCrudServer/SimpleCrudServer/Entities/Values.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCrudServer.Entities
+{
+    public class Values
+    {
+        public string value1 { get; set; }
+
+        public string value2 { get; set; }
+    }
+}
---
+++ simple-crud-client/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const API = "api/";
+const QUERY = "values";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      values: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(API + QUERY)
+      .then(response => response.json())
+      .then(data => this.setState({ values: data }));
+  }
+
+  render() {
+    const { values } = this.state;
+
+    return (
+      <div>
+      {values.map(value =>
+        <div key={value.value1}>
+          {value.value1} {value.value2}
+        </div>
+      )}
+    </div>
+    );
+  }
+}
+
+export default App;
